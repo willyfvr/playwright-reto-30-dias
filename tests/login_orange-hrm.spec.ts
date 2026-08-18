1fd2df3a17896c2e5ccc0c9568428e37a290c7e8
Victor Fattor <victor.fattor@bosonic.digital>
2026-08-18 13:23:09 -0300

    add test to invalid username

--- tests/login_orange-hrm.spec.ts
+++ tests/login_orange-hrm.spec.ts
@@ -16,3 +16,16 @@ test("login Orange HRM", async ({ page }) => {
   await expect(page.getByRole('heading', {name: 'Dashboard'})).toBeVisible()
 
 })
+
+test("login Orange HRM invalid user", async ({ page }) => {
+
+
+  await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+  await page.getByRole('textbox', {name: 'Username'}).fill('pototito');
+  await page.getByRole('textbox', {name: 'Password'}).fill('admin123');
+  await page.getByRole('button', {name: 'Login'}).click();
+
+  await expect(page.getByText("Invalid Credentials")).toBeVisible()
+
+})
