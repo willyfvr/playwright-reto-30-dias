92d86aee2a6832a4ea61b7fbf849d417917b0209
Victor Fattor <victor.fattor@bosonic.digital>
2026-08-18 12:56:10 -0300

    change 1 completed - login to orangeHrm

--- tests/login_orange-hrm.spec.ts
+++ tests/login_orange-hrm.spec.ts
@@ -1,4 +1,4 @@
-import {test} from "@playwright/test";
+import {expect, test} from "@playwright/test";
 
 test("login Orange HRM", async ({ page }) => {
 
@@ -9,4 +9,10 @@ test("login Orange HRM", async ({ page }) => {
   await page.getByRole('textbox', {name: 'Password'}).fill('admin123');
   
   await page.getByRole('button', {name: 'Login'}).click();
+
+  // now we need to validate that login was successful
+
+  // role: heading
+  await expect(page.getByRole('heading', {name: 'Dashboard'})).toBeVisible()
+
 })
